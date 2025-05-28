db03c8096e8e40e7e341916e67efb2aff07f38bf
fastus201 <fabiobotta4@gmail.com>
2025-05-28 09:33:58 +0200

    aggiunto main + requirements.txt

---
+++ main.py
@@ -0,0 +1,34 @@
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+from sklearn.linear_model import SGDClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+import pandas as pd
+import re
+
+data = pd.read_csv(
+  '/PATH-TO-DATA/train.csv',
+  names=['sentiment', 'title', 'review']
+)
+
+# Access the corpus and target variables
+X = data.review
+y = data.sentiment.replace({1:'Negative', 2:'Positive'})
+
+# train test splitting
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+
+# We'll use this function to replace numbers from the string
+preprocessor = lambda text: re.sub(r'[^a-z ]', '', text.lower())
+
+# construct the pipeline with the procedural steps to
+# process the data and cast predictions
+pipe = Pipeline([
+  ('vec', CountVectorizer(stop_words='english', min_df=1000, preprocessor=preprocessor)),
+  ('tfid', TfidfTransformer()),
+  ('lr', SGDClassifier(loss='log'))
+])
+
+# fit the model to the data
+model = pipe.fit(X_train, y_train)
